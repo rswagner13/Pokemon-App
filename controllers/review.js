889ede00951788e7da28b8ab89e7f16c7166a46a
Mour Wagner <mourwagner13@gmail.com>
2024-01-17 23:08:54 -0500

    Added auto carousel to home page

--- controllers/review.js
+++ controllers/review.js
@@ -13,3 +13,15 @@ const router = express.Router();
 /* Require the db connection, and models
 --------------------------------------------------------------- */
 const db = require('../models');
+
+
+/* Routes
+---------------------------------------------------------------- */
+router.get('/', (req, res) => {
+    res.send("Reviews Page will be here")
+})
+
+
+/* Export these routes so that they are accessible in `server.js`
+--------------------------------------------------------------- */
+module.exports = router
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = require('./models');
 /* Require the routes in the controllers folder
 --------------------------------------------------------------------- */
 const pkmnCtrl = require('./controllers/pokemon');
-// const reviewCtrl = require('./controllers/review');
+const reviewCtrl = require('./controllers/review');
 
 
 /* Create the Express app
@@ -85,7 +85,7 @@ app.get('/about', function (req,res) {
 app.use('/pokemon', pkmnCtrl);
 // // This tells our app to look at the `controllers/review.js` file 
 // // to handle all routes that begin with `localhost:3000/review`
-// app.use('/review', reviewCtrl);
+app.use('/review', reviewCtrl);
 
 
 // The "catch-all" route: Runs for any other URL that doesn't match the above routes
